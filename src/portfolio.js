0f198611d6ca88e93a179def7820384edf996b26
Salman Barick M <107459965+Salman-06@users.noreply.github.com>
2025-01-20 20:22:00 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -262,7 +262,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+91 8344655555",
+  number: "8344655555",
   email_address: "salmanshaa1825@gmail.com"
 };
 
