32f43a1faddb6f0e68f1b1d4104127bcd312feb3
Salman Barick M <107459965+Salman-06@users.noreply.github.com>
2025-01-20 20:54:24 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -258,11 +258,10 @@ const resumeSection = {
 };
 
 const contactInfo = {
-  display: true, // Set true to display this section, defaults to false
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "8344655555",
+  number: "+91-8344655555",
   email_address: "salmanshaa1825@gmail.com"
 };
 
