26978e4095ef3c90935811031429ba64879c52d1
Salman Barick M <107459965+Salman-06@users.noreply.github.com>
2025-01-22 10:10:12 +0530

    Update Contact.js

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -30,23 +30,25 @@ export default function Contact() {
               }
             >
               {contactInfo.number && (
-                <>
-                  <a
-                    className="contact-detail"
-                    href={"tel:" + contactInfo.number}
-                  >
-                    {contactInfo.number}
-                  </a>
-                  <br />
-                  <br />
-                </>
-              )}
-              <a
-                className="contact-detail-email"
-                href={"mailto:" + contactInfo.email_address}
-              >
-                {contactInfo.email_address}
-              </a>
+              <>
+                <h3 className="contact-detail-header">Phone:</h3>
+                <a
+                  className="contact-detail"
+                  href={"tel:" + contactInfo.number}
+                >
+                  {contactInfo.number}
+                </a>
+                <br />
+                <br />
+              </>
+            )}
+            <h3 className="contact-detail-header">Email:</h3>
+            <a
+              className="contact-detail-email"
+              href={"mailto:" + contactInfo.email_address}
+            >
+              {contactInfo.email_address}
+            </a>
               <br />
               <br />
               <SocialMedia />
