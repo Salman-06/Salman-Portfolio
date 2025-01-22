9bc65812a81986a84e379623dc4d77077d54b303
Salman Barick M <107459965+Salman-06@users.noreply.github.com>
2025-01-22 09:40:47 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -234,6 +234,8 @@ const achievementSection = {
 const resumeSection = {
   title: "Resume",
   subtitle: "Feel free to download my resume",
+  // Please Provide with Your Podcast embeded Link
+  display: true // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
